5ff10445abf7571de5a94e87563da1ba4bbcc33f
Oliver Praesto <oliver.praesto@gmail.com>
2023-01-24 23:10:55 +0100

    - adds mocked form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<script>
+<script lang="ts">
+  import { enhance } from '$app/forms';
+
+  let email = '';
+  let password = '';
 </script>
 
 <svelte:head>
@@ -7,3 +11,17 @@
 </svelte:head>
 
 <h1>Login</h1>
+
+<article>
+  <form method="POST" use:enhance>
+    <div class="grid">
+      <div>
+        <input placeholder="E-Mail" type="text" bind:value={email} />
+      </div>
+      <div>
+        <input placeholder="Password" type="text" bind:value={password} />
+      </div>
+    </div>
+    <button class="outlined">Login</button>
+  </form>
+</article>
