6d6857af0f0b3754bb52c771045a9e2b83ae1773
Oliver Praesto <oliver.praesto@gmail.com>
2023-01-24 23:13:36 +0100

    - adds content for secret page

--- src/routes/secret/+page.svelte
+++ src/routes/secret/+page.svelte
@@ -1,1 +1,14 @@
-<h1>Secret</h1>
+<h1 class="center">You're in!</h1>
+
+<div class="center">
+  <img
+    src="https://media0.giphy.com/media/3otPoS81loriI9sO8o/giphy.gif?cid=ecf05e47hc5agt77e1vzxptyf3evr9of6y6cmgima5hkt2ut&rid=giphy.gif&ct=g"
+    alt="Congratulations"
+  />
+</div>
+
+<style>
+  .center {
+    text-align: center;
+  }
+</style>
